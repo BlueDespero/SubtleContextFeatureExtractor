47ba99a2882e0f6c1920a19bdbf4dbf844a9b110
BlueDespero <cezarytroska@gmail.com>
2022-06-11 11:18:56 +0200

    Adding an interface data sources should implement.

---
+++ data/utils/DataSourceInterface.py
@@ -0,0 +1,19 @@
+class Translation:
+    def __init__(self):
+        self.no_lines = None
+        self.no_paragraphs = None
+
+    def get_line(self, line_id):
+        raise NotImplemented
+
+    def get_paragraph(self, paragraph_id):
+        raise NotImplemented
+
+
+class DataSource:
+
+    def __init__(self):
+        self.no_translations = None
+
+    def get_translation(self, translation_id) -> Translation:
+        raise NotImplemented
