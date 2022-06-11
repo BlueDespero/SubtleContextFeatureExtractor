d0573c5fc3c07d17f17d0136ef1d1a0c28095728
AntoniDabrowski <38124281+AntoniDabrowski@users.noreply.github.com>
2022-06-11 20:10:29 +0200

    New method for computing a paragraph embedding

--- data_preprocessing/paragraph_matching/paragraph_embedding.py
+++ data_preprocessing/paragraph_matching/paragraph_embedding.py
@@ -1,6 +1,8 @@
 import gensim.downloader
-from gensim.corpora.wikicorpus import WikiCorpus, tokenize
+from gensim.corpora.wikicorpus import tokenize
 import numpy as np
+from collections import defaultdict as dd
+from nltk.corpus import stopwords
 
 
 class Word2vec_matching:
@@ -11,29 +13,62 @@ class Word2vec_matching:
         # paragraph embedding is sum of word (from w2v) embeddings
         def paragraph_vector(paragraph):
             list_of_tokens = tokenize(paragraph)
-            return sum([self.model[word] for word in list_of_tokens[:100] if word in self.model.key_to_index])
+            return sum([self.model[word] for word in list_of_tokens if word in self.model.key_to_index])
 
         v_1 = paragraph_vector(paragraph_1)
         v_2 = paragraph_vector(paragraph_2)
         return np.dot(v_1, v_2) / (np.linalg.norm(v_1) * np.linalg.norm(v_2))
 
     def similarity_2(self, paragraph_1, paragraph_2):
-        stopwords = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
-                     "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its",
-                     "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this",
-                     "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has",
-                     "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or",
-                     "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between",
-                     "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in",
-                     "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when",
-                     "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some",
-                     "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can",
-                     "will", "just", "don", "should", "now"]
         def paragraph_vector(paragraph):
             list_of_tokens = tokenize(paragraph)
-            return sum([self.model[word] for word in list_of_tokens[:100]
-                        if word in self.model.key_to_index and len(word)>3 and word not in stopwords])
+            return sum([self.model[word] for word in list_of_tokens
+                        if word in self.model.key_to_index and len(word)>3 and word not in stopwords.words('english')] +
+                       [np.zeros(300).astype(np.float32)])
 
         v_1 = paragraph_vector(paragraph_1)
         v_2 = paragraph_vector(paragraph_2)
-        return np.dot(v_1, v_2) / (np.linalg.norm(v_1) * np.linalg.norm(v_2))
+        normalization = (np.linalg.norm(v_1) * np.linalg.norm(v_2))
+        if normalization != 0.0:
+            return np.dot(v_1, v_2) / normalization
+        else:
+            return 0.0
+
+    def similarity_4(self, paragraph_1, paragraph_2):
+        value_1 = self.similarity_2(paragraph_1, paragraph_2)
+        value_2 = similarity_3(paragraph_1, paragraph_2)
+        return value_1 * value_2
+
+def similarity_3(paragraph_1, paragraph_2):
+    def paragraph_preprocessing(paragraph):
+        list_of_tokens = tokenize(paragraph)
+        return [word for word in list_of_tokens if len(word)>3 and word not in stopwords.words('english')]
+
+    p_1 = paragraph_preprocessing(paragraph_1)
+    p_2 = paragraph_preprocessing(paragraph_2)
+    d_1 = dd(int)
+    d_2 = dd(int)
+
+
+    for element in p_1:
+        d_1[element] += 1
+    for element in p_2:
+        d_2[element] += 1
+    union = 0
+    visited = set()
+    for key in d_1:
+        if key not in visited:
+            visited.add(key)
+            union += max(d_1[key],d_2[key])
+
+    for key in d_2:
+        if key not in visited:
+            visited.add(key)
+            union += max(d_1[key],d_2[key])
+
+    intersection = len([value for value in p_1 if value in p_2])
+
+    if union == 0:
+        return 0.0
+    else:
+        return intersection/union
