7a1340dfff4bb706317646e6087c38919e4f4540
BlueDespero <cezarytroska@gmail.com>
2022-06-11 14:56:54 +0200

    Index translations from 0

--- data/quran/utils/indexing_translation.sh
+++ data/quran/utils/indexing_translation.sh
@@ -1,8 +1,8 @@
 LAST=$(exec find ../translations -regex "../translations/[0-9].*" -exec basename {} \; | sed 's/\([0-9]\+\).*/\1/g' | sort -n | tail -1)
-LAST=${LAST:-0}
+[ -z "$LAST" ] && LAST=0 || LAST=$((LAST+1))
 
 while read trans;
 do
+  mv ../translations/"$trans" ../translations/"$(printf "%03d" $((LAST)))"-"$(echo "$trans" | cut -d- -f2)"
   LAST=$((1+LAST))
-  mv ../translations/"$trans" ../translations/"$(printf "%03d" $(( 1 + LAST)))"-"$(echo "$trans" | cut -d- -f2)"
 done < <(ls ../translations/eng*.txt)
