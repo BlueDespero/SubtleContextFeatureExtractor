15e2d34ccaae60c0883d0c4883c4100f0535002d
BlueDespero <cezarytroska@gmail.com>
2022-06-25 08:35:33 +0200

    Add placeholder Data Source object for the books which are still missing it. It is necessary for get_data_source_object_from_name.

---
+++ data/The_Count_of_Monte_Cristo/CountDataSource.py
@@ -0,0 +1,9 @@
+from data.utils.DataSourceInterface import Translation, DataSource
+
+
+class CountTranslation(Translation):
+    pass
+
+
+class CountDataSource(DataSource):
+    pass
--- data/utils/dataloaders.py
+++ data/utils/dataloaders.py
@@ -1,11 +1,34 @@
 from typing import List
 
+from data.The_Count_of_Monte_Cristo.CountDataSource import CountDataSource
+from data.bible.BibleDataSource import BibleDataSource
+from data.les_miserables.LesMiserablesDataSource import LesMiserablesDataSource
+from data.madame_bovary.MadameBovaryDataSource import MadameBovaryDataSource
+from data.quran.QuranDataSource import QuranDataSource
+from data.utils.DataSourceInterface import DataSource
+
+
+def get_data_source_object_from_name(book_name: str) -> DataSource:
+    match book_name.lower():
+        case 'quran':
+            return QuranDataSource()
+        case 'bible':
+            return BibleDataSource()
+        case 'les miserables':
+            return LesMiserablesDataSource()
+        case 'madame bovary':
+            return MadameBovaryDataSource()
+        case 'the count of monte cristo':
+            return CountDataSource()
+        case _:
+            raise KeyError("{} - no such book in the database".format(book_name))
+
 
 class Dataloader:
-    def __init__(self, 
-                 book_name: str, 
-                 book_translations: List[int], 
-                 batch_size: int, 
+    def __init__(self,
+                 book_name: str,
+                 book_translations: List[int],
+                 batch_size: int,
                  shuffle=True,
                  device='cpu') -> None:
         self.book_name = book_name
@@ -13,6 +36,8 @@ class Dataloader:
         self.batch_size = batch_size
         self.shuffle = shuffle
         self.device = device
+
+        self._data_source = get_data_source_object_from_name(self.book_name)
 
     def __len__(self) -> int:
         pass
