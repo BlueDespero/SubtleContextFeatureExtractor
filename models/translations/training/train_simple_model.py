d07aa88ce3a8d339653a45518a8646fbd53470d1
AntoniDabrowski <38124281+AntoniDabrowski@users.noreply.github.com>
2022-06-28 00:33:22 +0200

    Further training

--- models/translations/training/train_simple_model.py
+++ models/translations/training/train_simple_model.py
@@ -8,6 +8,7 @@ from models.translations.combined_models.simple_model import training, MyEnsembl
 if __name__ == '__main__':
     print("Loading data...")
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f'Device: {device}')
     # device = 'cpu'
     l = [0,1,2,3]
     init = True
@@ -18,13 +19,14 @@ if __name__ == '__main__':
                                            training_proportion=0.7,
                                            testing_proportion=0.15,
                                            validation_proportion=0.15,
-                                           batch_size=5,
+                                           batch_size=32,
                                            embedding='bert',
                                            shuffle=True,
                                            device='cpu')
         print('Model initialization...')
         if init:
             feature_extractor = LSTM()
+            feature_extractor.load_state_dict(torch.load(r'..\trained_models\FE_bert_1.model'))
             feature_extractor.to(device)
             init = False
         else:
@@ -35,5 +37,5 @@ if __name__ == '__main__':
         model = MyEnsemble(feature_extractor, classifier)
 
         print('Training...')
-        training(model, data_loader, log_every=2,learning_rate=0.1,device=device,max_num_epochs=2)
+        training(model, data_loader, log_every=3,learning_rate=0.01,device=device,max_num_epochs=4)
         torch.save(feature_extractor.state_dict(), r'..\trained_models\FE_bert.model')
