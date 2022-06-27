029f6bf2820a483bb44fa0f20de25a3edabba40d
BlueDespero <cezarytroska@gmail.com>
2022-06-27 19:56:39 +0200

    Update TheWitcherDataSource.py from base

--- data/hypothesis_testing/the_witcher/TheWitcherDataSource.py
+++ data/hypothesis_testing/the_witcher/TheWitcherDataSource.py
@@ -3,53 +3,53 @@ import os
 from data.utils.DataSourceBase import BaseDataSource, BaseMetadata, BaseMatchingTranslation
 from definitions import ROOT_DIR
 
-CENTRAL_EARTHSEA_TRANSLATION = os.path.join(ROOT_DIR, 'data', 'hypothesis_testing', 'earthsea', 'translations',
-                                                  '000-The_Wizard_of_the_Earthsea.txt')
+CENTRAL_WITCHER_TRANSLATION = os.path.join(ROOT_DIR, 'data', 'hypothesis_testing', 'the_witcher', 'translations',
+                                           '000-The Tower of Swallow.txt')
 
 
-class EarthseaTranslation(BaseMatchingTranslation):
+class TheWitcherTranslation(BaseMatchingTranslation):
     def __init__(self, path, translation_id, embedding):
-        self.path_to_mapping = os.path.join(ROOT_DIR, 'data', 'hypothesis_testing', 'earthsea', 'preprocessed_data',
+        self.path_to_mapping = os.path.join(ROOT_DIR, 'data', 'hypothesis_testing', 'the_witcher', 'preprocessed_data',
                                             'translation_{}_mapping.pickle'.format(translation_id))
         super().__init__(path, translation_id, embedding)
 
     def _get_central_translation(self):
-        return EarthseaTranslation(CENTRAL_EARTHSEA_TRANSLATION, 0, None)
+        return TheWitcherTranslation(CENTRAL_WITCHER_TRANSLATION, 0, None)
 
     def _map_paragraphs(self):
-        if self.path == CENTRAL_EARTHSEA_TRANSLATION:
+        if self.path == CENTRAL_WITCHER_TRANSLATION:
             self._solo_paragraph_mapping()
         else:
             self._central_paragraph_mapping()
 
 
-class EarthseaMetadata(BaseMetadata):
+class TheWitcherMetadata(BaseMetadata):
 
     def __init__(self, data_source):
-        path_to_save_file = os.path.join(ROOT_DIR, 'data', 'hypothesis_testing', 'earthsea', 'preprocessed_data',
+        path_to_save_file = os.path.join(ROOT_DIR, 'data', 'hypothesis_testing', 'the_witcher', 'preprocessed_data',
                                          'metadata.pickle')
         super().__init__(data_source, path_to_save_file)
 
 
-class EarthseaDataSource(BaseDataSource):
+class TheWitcherDataSource(BaseDataSource):
     def __init__(self):
         super().__init__()
         self.all_translations_list = os.listdir(
-            os.path.join(ROOT_DIR, 'data', 'hypothesis_testing', 'earthsea', "translations"))
+            os.path.join(ROOT_DIR, 'data', 'hypothesis_testing', 'the_witcher', "translations"))
         self.no_translations = len(self.all_translations_list)
 
-    def get_translation(self, translation_id: int, embedding=None) -> EarthseaTranslation:
+    def get_translation(self, translation_id: int, embedding=None) -> TheWitcherTranslation:
         if 0 <= translation_id < self.no_translations:
             chosen_translation_path = self._find_translation(translation_id)
-            return EarthseaTranslation(chosen_translation_path, translation_id, embedding)
+            return TheWitcherTranslation(chosen_translation_path, translation_id, embedding)
         else:
             raise IndexError
 
     def _find_translation(self, translation_id: int):
         for translation_name in self.all_translations_list:
             if translation_id == int(translation_name.split("-")[0]):
-                return os.path.join(ROOT_DIR, 'data', 'hypothesis_testing', 'earthsea', "translations",
+                return os.path.join(ROOT_DIR, 'data', 'hypothesis_testing', 'the_witcher', "translations",
                                     translation_name)
 
-    def get_metadata(self) -> EarthseaMetadata:
-        return EarthseaMetadata(data_source=self)
+    def get_metadata(self) -> TheWitcherMetadata:
+        return TheWitcherMetadata(data_source=self)
